466e5436ac28dd75af7c60371b6f62f0f3d9902c
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-31 15:09:23 +0200

    Added Overlay Menu on Mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 (function() {
 
-    
+
+    function openNav() {
+        $("nav").css('width', '100%');
+    }
+
+    function closeNav() {
+        $("nav").css('width', '0%');
+    }
+
+    /* Open when someone clicks on the Hamburger button */
+    $('#hamburger').on('click', function() {
+        openNav();
+    });
+
+    /* Close when someone clicks on the "x" symbol inside the mobile menu */
+    $('.closebtn').on('click', function() {
+        closeNav();
+    });
 
 }());
